6652af996692afb61f7ec45138476833734bfb09
Emil Müller <160159962+KSWInformatik@users.noreply.github.com>
2025-04-25 22:31:30 +0200

    Update getbook.php

--- app-code/app/getbook.php
+++ app-code/app/getbook.php
@@ -118,11 +118,11 @@
       
       $title = $book['title'];
       $pages = $book['pages'];
-      $author = "";
-      for_each( $book['authors'] as $key => $value){
-        $author .= $value." / ";
-      }
-      echo $author;
+      // $author = "";
+      // for_each( $book['authors'] as $key => $value){
+      //   $author .= $value." / ";
+      // }
+      // echo $author;
 
     curl_close($rest);
 
