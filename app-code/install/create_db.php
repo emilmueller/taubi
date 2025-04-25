2b8121a84c3feeaab93468347ced31d54ea72455
Janis Steiner <janis@jakach.ch>
2025-04-25 17:02:17 +0200

    updating db creation

--- app-code/install/create_db.php
+++ app-code/install/create_db.php
@@ -60,6 +60,7 @@
 			$sql="CREATE TABLE IF NOT EXISTS users (
 			    id INT AUTO_INCREMENT PRIMARY KEY,
 			    username VARCHAR(255) NOT NULL UNIQUE,
+			    role VARCHAR(255),
 			    email VARCHAR(255) NOT NULL UNIQUE,
 			    last_login VARCHAR(255)
 			);";
