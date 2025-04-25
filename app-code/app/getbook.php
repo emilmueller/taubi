1e384faca9b956655092e2b9cc5828d7be6d7167
Emil Müller <160159962+KSWInformatik@users.noreply.github.com>
2025-04-25 22:42:38 +0200

    Update getbook.php

--- app-code/app/getbook.php
+++ app-code/app/getbook.php
@@ -84,6 +84,7 @@
 
     echo "<pre>";
     print_r($book);
+    echo "</pre>";
     
     
 
@@ -113,7 +114,7 @@
 
 
       $sql = "insert into books (title, pages, author, publisher, language, book_condition, image_url, reserved_by, date_published, isbn,price) VALUES ( ?,?,?,?,?,?,?,?,?,?,?)";
-      $stmt = $conn->prepare($sql);
+      // $stmt = $conn->prepare($sql);
       // $stmt->bind_param("sisssssisss", $title, $pages, $author, $publisher, $language, $book_condition, $image_url, $reserved_by, $date_published, $isbn, $price);
       
       $title = $book['title'];
@@ -129,7 +130,7 @@
 
     curl_close($rest);
 
-    echo "</pre>";
+    
 
     ?>  
 </body>
