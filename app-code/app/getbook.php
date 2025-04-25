c40bb527d34117a68f29df76789da1cff7131ea3
Emil Müller <160159962+KSWInformatik@users.noreply.github.com>
2025-04-25 22:58:04 +0200

    Update getbook.php

--- app-code/app/getbook.php
+++ app-code/app/getbook.php
@@ -121,16 +121,16 @@
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("sisssssisss", $title, $pages, $author, $publisher, $language, $book_condition, $image_url, $reserved_by, $date_published, $isbn, $price);
       
-    $title = $book['title'];
-    $pages = $book['pages'];
-    $author = "";
-    foreach($book['authors'] as $key => $value){
-      $author .= $value." / ";
-    }
-    $author =substr($author, 0, -3);
-    echo $author;
-    $publisher= $book['publisher'];
-    $language = $book]'language'];
+    // $title = $book['title'];
+    // $pages = $book['pages'];
+    // $author = "";
+    // foreach($book['authors'] as $key => $value){
+    //   $author .= $value." / ";
+    // }
+    // $author =substr($author, 0, -3);
+    // echo $author;
+    // $publisher= $book['publisher'];
+    // $language = $book]'language'];
     // $book_condition = $book['book']
 
 
