60d1eefe9b3b44e8a37a7be238f9d7e0471e01d4
Emil Müller <160159962+KSWInformatik@users.noreply.github.com>
2025-05-05 17:55:09 +0200

    Update getbook.php

--- app-code/app/getbook.php
+++ app-code/app/getbook.php
@@ -160,8 +160,10 @@
     <!-- Search + Fach Dropdown -->
     <div class="book-form row">
       <div class="mb-2">
-        <label for="authorInput" class="col-form-label col-md-2">Autor:in</label>
-        <div class="col-md-10">
+        <div class="col-auto">
+          <label for="authorInput" class="col-form-label">Autor:in</label>
+        </div>
+        <div class="col-auto">
           <input type="text" id="authorInput" class="form-control" placeholder="Autor:in" value=<?php echo $author ?>/>
         </div>
       </div>
