e12572208faa6ba6edb97f7562d9df0886e3778c
Janis Steiner <janis@jakach.ch>
2025-05-05 15:29:41 +0000

    .

--- app-code/account/index.php
+++ app-code/account/index.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+
+// Check if the user is logged in
+if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
+    header("LOCATION:/login/login.php");
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html data-bs-theme="dark" lang="de">
 <head>
--- app-code/api/get_tags.php
+++ app-code/api/get_tags.php
@@ -1,4 +1,16 @@
 <?php
+session_start();
+
+// Check if the user is logged in
+if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
+        $response = [
+            "error" => "not authorized"
+        ];
+        echo json_encode($response);
+    exit();
+}
+?>
+<?php
 include "../config.php";
 $sql = "SELECT name FROM tags";
 $result = $conn->query($sql);
