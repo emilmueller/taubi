98739d21646e93588a191d714b64d5e22bbd03b9
Emil Müller <160159962+KSWInformatik@users.noreply.github.com>
2025-05-06 07:47:26 +0200

    Update scan_barcode.php

--- app-code/app/scan_barcode.php
+++ app-code/app/scan_barcode.php
@@ -68,6 +68,18 @@
 </head>
 
 <body>
+  <!-- Ribbon at the top -->
+  <div class="ribbon d-flex justify-content-between align-items-center">
+      <div>
+        <a href="/" class="btn btn-link">Bibliothek</a>
+        <a href="/account?my_books" class="btn btn-link">Meine Bücher</a>
+      </div>
+      <div class="d-flex align-items-center">
+        <a href="/account" class="btn btn-link">
+          <i class="bi bi-person-circle"></i> Konto
+        </a>
+      </div>
+    </div>
 
   <main class="wrapper" style="padding-top:2em">
 
