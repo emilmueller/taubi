9087cc2f54df42789799e38f82762abb1c6fd846
Emil Müller <160159962+KSWInformatik@users.noreply.github.com>
2025-05-08 16:12:37 +0200

    Create add_user.php

---
+++ app-code/api/add_user.php
@@ -0,0 +1,13 @@
+<?php
+
+include "../config.php";
+$sql = "
+insert into users (username, email) VALUES (`ueli`, `ueli@ksw.ch`);"
+    
+$result = $conn->query($sql);
+
+
+
+// Close connection
+$conn->close();
+?>
