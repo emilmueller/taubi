104083f413a1c11b60d030a63047928d83146bf0
Emil Müller <160159962+KSWInformatik@users.noreply.github.com>
2025-05-08 16:07:34 +0200

    get_users.php added

--- app-code/api/get_users.php
+++ app-code/api/get_users.php
@@ -14,33 +14,24 @@ if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
 
 include "../config.php";
 $sql = "
-SELECT
-    b.*,
-    u.username AS seller_name,
-    JSON_ARRAYAGG(t.name) AS tags
-FROM books b
-LEFT JOIN users u ON b.sold_by = u.id
-LEFT JOIN book_tags bt ON b.id = bt.book_id
-LEFT JOIN tags t ON bt.tag_id = t.id
-GROUP BY b.id;
-";
+SELECT * from users;";
+    
 
 $result = $conn->query($sql);
 
 // Result array
-$books = [];
+$users = [];
 
 if ($result && $result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        // Decode tags JSON array to actual array (optional)
-        $row['tags'] = json_decode($row['tags']);
-        $books[] = $row;
+               
+        $users[] = $row;
     }
 }
 
 // Output JSON
 header('Content-Type: application/json');
-echo json_encode($books, JSON_PRETTY_PRINT);
+echo json_encode($users, JSON_PRETTY_PRINT);
 
 // Close connection
 $conn->close();
