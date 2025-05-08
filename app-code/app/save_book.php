86cfb5e0e0f5b35c036f008ed651e92c11b30c6e
Emil Müller <160159962+KSWInformatik@users.noreply.github.com>
2025-05-08 15:10:34 +0200

    Update save_book.php

--- app-code/app/save_book.php
+++ app-code/app/save_book.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 session_start();
 
 // Check if the user is logged in
@@ -9,7 +9,7 @@ if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
         echo json_encode($response);
     exit();
 }
-?>
+?> -->
 <?php 
     include "../config.php";
 
