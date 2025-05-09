bcca0adea048113eafd43e33702f7b82a0fcc243
Emil Müller <160159962+KSWInformatik@users.noreply.github.com>
2025-05-09 14:08:25 +0200

    Update save_book.php

--- app-code/app/save_book.php
+++ app-code/app/save_book.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
 <?php 
     include "../config.php";
 
-    $imageData = file_get_contents($image_url);
+    $imageData = file_get_contents($_POST['image_url']);
     if ($imageData === FALSE) { 
         die("Could not fetch image from the URL."); 
     } 
