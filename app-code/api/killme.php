bf4063e5be402f480a24d23a25001dfeb20f7dbc
Emil Müller <160159962+KSWInformatik@users.noreply.github.com>
2025-05-09 15:19:15 +0200

    Update killme.php

--- app-code/api/killme.php
+++ app-code/api/killme.php
@@ -3,42 +3,42 @@
 <?php 
     include "../config.php";
 
-    // $sql = "SELECT image_data,image_url FROM books where id=".$_GET['id'].";";
-    // $res = $conn->query($sql);
+    $sql = "SELECT image_data,image_url FROM books where id=".$_GET['id'].";";
+    $res = $conn->query($sql);
 
-    // $row = $res->fetch_assoc();
+    $row = $res->fetch_assoc();
 
-    // header('Content-Type: image/jpeg');
+    header('Content-Type: image/jpeg');
     
-    // echo $row['image_data'];
+    echo $row['image_data'];
 
 
-    $imageData = file_get_contents("https://sample-videos.com/img/Sample-jpg-image-100kb.jpg");
-    if ($imageData === FALSE) { 
-        die("Could not fetch image from the URL."); 
-    } 
+    // $imageData = file_get_contents("https://sample-videos.com/img/Sample-jpg-image-100kb.jpg");
+    // if ($imageData === FALSE) { 
+    //     die("Could not fetch image from the URL."); 
+    // } 
 
-    $sql = "insert into books (title, pages, author, publisher, language,  image_url, date_published, isbn, book_condition, price, sold_by, image_data ) VALUES ( ?,?,?,?,?,?,?,?,?,?,?,?);";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sissssssssib", $title, $pages, $author, $publisher, $language, $image_url,  $date_published, $isbn, $book_condition, $price, $_SESSION['id'], $imageData);
+    // $sql = "insert into books (title, pages, author, publisher, language,  image_url, date_published, isbn, book_condition, price, sold_by, image_data ) VALUES ( ?,?,?,?,?,?,?,?,?,?,?,?);";
+    // $stmt = $conn->prepare($sql);
+    // $stmt->bind_param("sissssssssib", $title, $pages, $author, $publisher, $language, $image_url,  $date_published, $isbn, $book_condition, $price, $_SESSION['id'], $imageData);
     
-    $title = "TEST";
+    // $title = "TEST";
 
-    $pages = 6;
-    $author = "NOBODY";
+    // $pages = 6;
+    // $author = "NOBODY";
     
-    $publisher= "Big Mac";
+    // $publisher= "Big Mac";
 
-    $language = "de";
+    // $language = "de";
 
-    $image_url = "https://sample-videos.com/img/Sample-jpg-image-100kb.jpg";
-    $date_published ="2055";
-    $isbn = "124509234";
-    $book_condition = "top";
-    $price = "cheap";
+    // $image_url = "https://sample-videos.com/img/Sample-jpg-image-100kb.jpg";
+    // $date_published ="2055";
+    // $isbn = "124509234";
+    // $book_condition = "top";
+    // $price = "cheap";
 
 
-    $stmt->execute();
+    // $stmt->execute();
 
 
 
