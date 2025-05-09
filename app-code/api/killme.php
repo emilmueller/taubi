679adce08e0b8f5f04a70327ae0cbe5c944ad0a0
Emil Müller <160159962+KSWInformatik@users.noreply.github.com>
2025-05-09 14:26:04 +0200

    Update killme.php

--- app-code/api/killme.php
+++ app-code/api/killme.php
@@ -1,7 +1,7 @@
 <?php 
     include "../config.php";
 
-    $sql = "SELECT image FROM books where id=38;";
+    $sql = "SELECT image FROM books where id=".$_GET['id'].";";
     $res = $conn->query($sql);
 
     $row = $res->fetch_assoc();
