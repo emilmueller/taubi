5a7c5ad7b5ae77ee335893442659cbcaa13b070a
Emil Müller <160159962+KSWInformatik@users.noreply.github.com>
2025-05-09 14:13:22 +0200

    Update killme.php

--- app-code/api/killme.php
+++ app-code/api/killme.php
@@ -1,7 +1,7 @@
 <?php 
     include "../config.php";
 
-    $imageData = file_get_contents($_POST['image_url']);
+    $imageData = file_get_contents("https://images.isbndb.com/covers/7521763483567.jpg");
     if ($imageData === FALSE) { 
         die("Could not fetch image from the URL."); 
     } 
