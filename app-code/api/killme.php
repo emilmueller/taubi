7e840ea8a7d2a2d81991a4b57fc51853330496a8
Emil Müller <160159962+KSWInformatik@users.noreply.github.com>
2025-05-09 15:24:42 +0200

    Update killme.php

--- app-code/api/killme.php
+++ app-code/api/killme.php
@@ -10,7 +10,7 @@
 
     //header('Content-Type: image/jpeg');
     
-    echo "Länge: ".len($row['image_data']);
+    echo "Länge: ".count($row['image_data']);
 
 
     // $imageData = file_get_contents("https://sample-videos.com/img/Sample-jpg-image-100kb.jpg");
