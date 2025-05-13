60b2878efdd36b4daa5d2988fd70dd95146678c6
Emil Müller <160159962+KSWInformatik@users.noreply.github.com>
2025-05-13 14:15:27 +0200

    Update fotoupload.php

--- app-code/api/fotoupload.php
+++ app-code/api/fotoupload.php
@@ -4,7 +4,7 @@
 header('Content-Type: application/json');
 
 // Verzeichnispfad für Uploads (schreibbar machen!)
-$uploadDir = 'bookcovers';
+$uploadDir = dirname(__DIR__).'/bookcovers/';
 error_log("------->".$uploadDir);
 
 
