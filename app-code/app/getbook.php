c69213f9011471b2948b156a99c0ed8fd85b10bf
Emil Müller <mfg@emil-mueller.ch>
2025-05-14 09:51:10 +0200

    Update getbook.php

--- app-code/app/getbook.php
+++ app-code/app/getbook.php
@@ -294,7 +294,7 @@ if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
     
 
   </div>
-  <script>
+  <!-- <script>
     const video = document.getElementById('video');
     const canvas = document.getElementById('canvas');
     const captureButton = document.getElementById('capture');
@@ -345,7 +345,7 @@ if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
     });
 
 
-  </script>
+  </script> -->
  
 
   
