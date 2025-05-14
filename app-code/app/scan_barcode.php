8e6bab3621e608356873466153080e4ababd8d5a
Emil Müller <mfg@emil-mueller.ch>
2025-05-14 16:46:31 +0200

    Update scan_barcode.php

--- app-code/app/scan_barcode.php
+++ app-code/app/scan_barcode.php
@@ -327,6 +327,21 @@
               sourceSelect.appendChild(sourceOption)
             })
 
+
+            codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
+              if (result) {
+                console.log(result)
+                document.getElementById('isbnInput').value = result.text
+                window.open("getbook.php?isbn="+result.text);
+              }
+              if (err && !(err instanceof ZXing.NotFoundException)) {
+                console.error(err)
+                document.getElementById('isbnInput').value = err
+              }
+            })
+            console.log(`Started continous decode from camera with id ${selectedDeviceId}`)
+
+
             sourceSelect.onchange = () => {
               selectedDeviceId = sourceSelect.value;
               codeReader.reset()
