67455e4d07970bc7d3a081a16f2185bf3b0db126
Emil Müller <mfg@emil-mueller.ch>
2025-05-14 16:36:39 +0200

    Update scan_barcode.php

--- app-code/app/scan_barcode.php
+++ app-code/app/scan_barcode.php
@@ -145,7 +145,7 @@
       left: 30%;
       width: 40%;
       height: 40%;
-      border: 1px dashed white;
+      border: 1px dashed grey;
       box-sizing: border-box;
       background-color: rgba(0, 0, 0, 0.1); /* leichte Transparenz */
     }
