5af373ddd88f32e94008545db7a698ea913e6ffa
Emil Müller <mfg@emil-mueller.ch>
2025-05-16 18:09:35 +0200

    Update index.php

--- app-code/app/index.php
+++ app-code/app/index.php
@@ -175,7 +175,7 @@ if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
               <p class="text-muted" style="display:none">ISBN: ${book.isbn}</p>
 	      <p class="text-muted">Verkäufer: ${book.seller_name}</p>
               <button class="btn btn-secondary" onclick="show_message_modal('${escapeForHtmlAttr(book.title)}','${book.seller_name}','${book.seller}');">Kontakt</button>
-	      <button class="btn btn-secondary" onclick="show_info_modal('${escapeForHtmlAttr(book.title)+"SOSO"}','${book.seller_name}','${book.author}','${book.pages}','${book.language}','${book.date_published}','${book.book_condition}','${book.price}','${book.image_url}');">Mehr Infos</button>
+	      <button class="btn btn-secondary" onclick="show_info_modal('${escapeForHtmlAttr(book.title)}','${book.seller_name}','${book.author}','${book.pages}','${book.language}','${book.date_published}','${book.book_condition}','${book.price}','${book.image_url}');">Mehr Infos</button>
             </div>
           </div>
         `;
