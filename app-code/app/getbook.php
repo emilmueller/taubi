d575b420cfe00045b7d13be935cad0a3b600fc0c
Emil Müller <160159962+KSWInformatik@users.noreply.github.com>
2025-05-16 15:31:48 +0200

    Update getbook.php

--- app-code/app/getbook.php
+++ app-code/app/getbook.php
@@ -290,7 +290,7 @@ if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
           },
           success: function(data){
             $("#spinner").hide();     // Spinner anzeigen
-            alert("HOLLA");
+            alert(data.title);
 
 
             $("#bookForm").show();      // Ergebnisbereich ausblenden
