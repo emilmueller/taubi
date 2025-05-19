8dbc2223d2cf614074690b0b3f5cc2f6b0e4986c
Emil Müller <160159962+KSWInformatik@users.noreply.github.com>
2025-05-19 17:37:56 +0200

    Update getbook.php

--- app-code/app/getbook.php
+++ app-code/app/getbook.php
@@ -229,7 +229,7 @@ if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
   <script>
     $(document).ready(function() {
         $.ajax({
-          let action = <?php echo $_GET['action']; ?>;
+          let action = <?php echo json_encode($_GET['action']); ?>;
           alert(action);
           if (action =="search_isbn"){
 
