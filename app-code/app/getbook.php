cecc83cde2875844ed3b546db7a4a2823cdddd78
Emil Müller <160159962+KSWInformatik@users.noreply.github.com>
2025-05-20 08:05:15 +0200

    Update getbook.php

--- app-code/app/getbook.php
+++ app-code/app/getbook.php
@@ -245,6 +245,7 @@ if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
             },
             success: function(response){
               $("#spinner").addClass('d-none');     // Spinner anzeigen
+              console.log(response);
               var res = $.parseJSON(response);
               $('#bookImage').attr('src', res.book.image);
               $('#image_url_input').val(res.book.image);
