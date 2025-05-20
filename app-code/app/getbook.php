3c4f25ee1d576d0aa09b8117c79e1cb63c9f941d
Emil Müller <160159962+KSWInformatik@users.noreply.github.com>
2025-05-20 09:33:53 +0200

    Update getbook.php

--- app-code/app/getbook.php
+++ app-code/app/getbook.php
@@ -214,7 +214,7 @@ if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
           </div>
           <div class="row align-items-center">
             <div class="col-4">
-                <button id="okButton" class="btn btn-secondary" onlick="submit()">Buch speichern</button>
+                <button id="okButton" class="btn btn-secondary float-right" onlick="submit()">Buch speichern</button>
             </div>
           </div>
           
