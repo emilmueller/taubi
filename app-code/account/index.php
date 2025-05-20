10017f96e5b8da6b0a297bba04f1544c9631c542
Emil Müller <160159962+KSWInformatik@users.noreply.github.com>
2025-05-20 10:00:57 +0200

    Update index.php

--- app-code/account/index.php
+++ app-code/account/index.php
@@ -173,8 +173,8 @@ if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
               <p class="text-muted" style="display:none">ISBN: ${book.isbn}</p>
             </div>
             <div class="card-footer">
-              <button class="btn btn-success" onclick="edit_book(${book.id})"><span class="bi bi-pencil"></span></button> <!-- edit Button -->
-              <button class="btn btn-danger" onclick="delete_book(${book.id})"><span class="bi bi-trash"></span></button> <!-- delete Button -->
+              <button class="btn btn-success float-end" onclick="edit_book(${book.id})"><span class="bi bi-pencil"></span></button> <!-- edit Button -->
+              <button class="btn btn-danger float-end" onclick="delete_book(${book.id})"><span class="bi bi-trash"></span></button> <!-- delete Button -->
             </div>
           
           </div>
