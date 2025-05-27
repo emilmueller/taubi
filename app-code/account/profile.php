ed0c52b4eb97dadd01eac4b1a1c5ab8d294ac351
Emil Müller <160159962+KSWInformatik@users.noreply.github.com>
2025-05-27 14:41:32 +0200

    Update profile.php

--- app-code/account/profile.php
+++ app-code/account/profile.php
@@ -162,7 +162,7 @@ if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
                 dataType: 'json',
                 success: function(response){
                   
-                  alert("RES: "+res.success);
+                  console.log(response);
                   // $('nameInput').val(res.user.)
               
 
