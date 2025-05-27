b949ab62966bc45604cf19753a998fb3744ada59
Emil Müller <160159962+KSWInformatik@users.noreply.github.com>
2025-05-27 14:23:45 +0200

    Update profile.php

--- app-code/account/profile.php
+++ app-code/account/profile.php
@@ -167,6 +167,7 @@ if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
 
                 },
                 error: function(response){
+                  console.log("EEEERROR");
 
 
                 }
